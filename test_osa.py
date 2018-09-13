5a32287a5a6a107e8fdd6a08810c3ca910602a07
Tom Barron <tusculum@gmail.com>
2018-09-13 16:19:08 -0400

    TW: Replace pexpect.run() with tbx.run()

--- test_osa.py
+++ test_osa.py
@@ -15,9 +15,9 @@ def test_count_notes_one():
     """
     The query string 'tag:current intitle:Start' should always find one note
     """
-    result = pexpect.run("count-notes 'intitle:\"Start Here\"'")
-    assert "notes: 1\r\n" in result.decode()
     pytest.dbgfunc()
+    result = tbx.run("count-notes 'intitle:\"Start Here\"'")
+    assert "notes: 1" in result.strip()
 
 
 # -----------------------------------------------------------------------------
@@ -25,9 +25,9 @@ def test_count_notes_zero():
     """
     The query string 'tag:precious tag:delete_me' should never find a note
     """
-    result = pexpect.run("count-notes 'tag:precious tag:delete_me'")
-    assert "notes: 0\r\n" in result.decode()
     pytest.dbgfunc()
+    result = tbx.run("count-notes 'tag:precious tag:delete_me'")
+    assert "notes: 0" in result.strip()
 
 
 # -----------------------------------------------------------------------------
@@ -38,11 +38,11 @@ def test_create_note():
     """
     pytest.dbgfunc()
     query = "'tag:test_create_note tag:delete_me intitle:Test'"
-    result = pexpect.run("count-notes {}".format(query)).decode().strip()
+    result = tbx.run("count-notes {}".format(query)).strip()
     found = re.findall(r"notes: (\d+)", result)
     count = int(found.pop())
     if 0 < count:
-        result = pexpect.run("del-notes -m {}".format(query)).decode().strip()
+        result = tbx.run("del-notes -m {}".format(query)).strip()
         found = re.findall(r"(\d+) notes deleted", result)
         deleted = int(found.pop())
         if count != deleted:
@@ -52,15 +52,15 @@ def test_create_note():
 
     cmd = ("create-note --title 'Test Note' --text 'this is a test' "
            "--tag 'test_create_note' --tag 'delete_me'")
-    result = pexpect.run(cmd).decode()
+    result = tbx.run(cmd)
     assert "note Test Note created" in result
 
     cmd = "count-notes {}".format(query)
-    result = pexpect.run(cmd).decode()
-    assert "notes: 1\r\n" in result
+    result = tbx.run(cmd)
+    assert "notes: 1" in result
 
     cmd = "del-notes {}".format(query)
-    result = pexpect.run(cmd).decode()
+    result = tbx.run(cmd)
     assert "1 notes deleted" in result
 
 
@@ -71,8 +71,8 @@ def test_del_notes_nomatch():
     """
     pytest.dbgfunc()
     cmd = "del-notes 'tag:no_such_note'"
-    result = pexpect.run(cmd)
-    assert "no notes match tag:no_such_note" in result.decode()
+    result = tbx.run(cmd)
+    assert "no notes match tag:no_such_note" in result
 
 
 # -----------------------------------------------------------------------------
@@ -84,16 +84,16 @@ def test_del_notes_nomult():
     """
     pytest.dbgfunc()
     cmd = "create-note --tag 'testing' --tag 'delete_me' --title "
-    result = pexpect.run(cmd + "'test note 1'")
-    assert "note test note 1 created" in result.decode()
-    result = pexpect.run(cmd + "'test note 2'")
-    assert "note test note 2 created" in result.decode()
+    result = tbx.run(cmd + "'test note 1'")
+    assert "note test note 1 created" in result
+    result = tbx.run(cmd + "'test note 2'")
+    assert "note test note 2 created" in result
 
-    result = pexpect.run("del-notes 'tag:testing tag:delete_me'")
-    assert "no notes deleted, --multiple required" in result.decode()
+    result = tbx.run("del-notes 'tag:testing tag:delete_me'")
+    assert "no notes deleted, --multiple required" in result
 
-    result = pexpect.run("del-notes -m 'tag:testing tag:delete_me'")
-    assert "2 notes deleted" in result.decode()
+    result = tbx.run("del-notes -m 'tag:testing tag:delete_me'")
+    assert "2 notes deleted" in result
 
 
 # -----------------------------------------------------------------------------
@@ -101,9 +101,8 @@ def test_make_date_basic():
     """
     make-date with arg '2017.0101' should work
     """
-    result = pexpect.run("make-date 2017.0101")
-    result = result.decode()
     pytest.dbgfunc()
+    result = tbx.run("make-date 2017.0101")
     assert "result = " not in result
     assert "2017.0102" in result
     assert re.match(r"\d{4}\.\d{4}\.\S\S\S", result)
@@ -114,9 +113,8 @@ def test_make_date_hhmm():
     """
     make-date with arg '2017.0217 13:29' should work
     """
-    result = pexpect.run("make-date '2017.0217 13:29'")
-    result = result.decode()
     pytest.dbgfunc()
+    result = tbx.run("make-date '2017.0217 13:29'")
     assert "result = " not in result
     assert re.match(r"2017\.0218\.sat", result)
 
@@ -126,9 +124,8 @@ def test_make_date_hhmmss():
     """
     make-date with arg '2017.0430 13:29:57' should work
     """
-    result = pexpect.run("make-date '2017.0430 13:29:57'")
-    result = result.decode()
     pytest.dbgfunc()
+    result = tbx.run("make-date '2017.0430 13:29:57'")
     assert "result = " not in result
     assert re.match(r"2017\.0501\.mon", result)
 
@@ -138,10 +135,10 @@ def test_make_date_noarg():
     """
     make-date with no arg spits out an error
     """
-    result = pexpect.run("make-date")
-    assert "execution error:".encode() in result
-    assert "t get item 1 of {}.".encode() in result
     pytest.dbgfunc()
+    result = tbx.run("make-date")
+    assert "execution error:" in result
+    assert "t get item 1 of {}." in result
 
 
 # -----------------------------------------------------------------------------
@@ -149,9 +146,8 @@ def test_make_date_wkday():
     """
     the output of make-date should be '2017.0606.tue'
     """
-    result = pexpect.run("make-date '2017.0606'")
-    result = result.decode()
     pytest.dbgfunc()
+    result = tbx.run("make-date '2017.0606'")
     assert "result = " not in result
     assert re.match(r"2017\.0607\.wed", result)
 
