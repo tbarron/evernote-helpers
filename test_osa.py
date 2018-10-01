9c23fb11e16aa053cf87a5aef4268597e0231a65
Tom Barron <tusculum@gmail.com>
2018-10-01 17:48:45 -0400

    TBX: Update test_releasable() to use tbx rather than pexpect

--- test_osa.py
+++ test_osa.py
@@ -168,7 +168,7 @@ def test_releasable():
     last_tag = tbx.git_last_tag()
 
     msg = "Version ({}) does not match tag ({})"
-    result = pexpect.run("version").decode().rstrip()
+    result = tbx.run("version").decode().rstrip()
     msg = msg.format(result, last_tag)
     assert result == last_tag, msg
 
